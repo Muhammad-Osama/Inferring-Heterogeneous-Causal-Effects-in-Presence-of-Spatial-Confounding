31c412aa6be80cfeee24f57addf1b4d5929341bb
Muhammad-Osama <37805794+Muhammad-Osama@users.noreply.github.com>
2021-02-08 13:25:04 +0100

    Update 1d_cosine_effect.m

--- 1d_cosine_effect.m
+++ 1d_cosine_effect.m
@@ -8,13 +8,13 @@ savedState=stream.State;
 n = 500; d = 1; 
 [y,z,s]= simulated_data(n,d);
 %true effect
-tau = @(u) cos(2*pi.*u./200);
+tau = @(u) cos(2*pi.*u./20);
 
 %% basis parameters
 %continuous space
 disc_or_cont = 'cont';
 
-Ns = 20; sup = [0.8 1.5]; mn = -5; mx = 105;
+Ns = 20; sup = [0.65 1.15]; mn = -5; mx = 20;
 
 %% estimate effect
 
@@ -35,10 +35,9 @@ figure;
 h = fill([se;flipud(se)], [tau_CI(:,1);flipud(tau_CI(:,2))],[0.5 .5 0.5]);
 set(h,'FaceAlpha',0.3);
 hold on;
-plot(se,tau(se),'r',se,tau_hat,'b','LineWidth',1.5);
+plot(se,tau(se),'r--',se,tau_hat,'b','LineWidth',1.5);
 xlabel('$s$','interpreter','Latex'); 
 legend({'$95\%~CI$','$\tau(s)$','$\widehat{\tau(s)}$'},'interpreter','Latex');
 grid on;
-
-
-
+xlim([0, 10])
+ylim([-1.5 1.5])
